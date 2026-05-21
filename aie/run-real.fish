b18c8e13ef752619e69765d4c09048c352adc8cb
Bugra Eryilmaz <bugra.eryilmaz@epfl.ch>
2026-05-21 16:13:20 +0200

    found backup xclbin for aie pagerank-pe1

--- aie/run-real.fish
+++ aie/run-real.fish
@@ -4,11 +4,11 @@ set -x ROOTDIR (pwd)
 # Build host executables
 cd "$ROOTDIR"/fib_pe1
 make host
-if test -e "$ROOTDIR"/pageRankv2_pe1_8/src/host/build/projects/pageRank/pageRank_xrt
-    echo "Executable for pageRankv2_pe1_8 already exists, skipping build"
+if test -e "$ROOTDIR"/pagerank-pe1/src/host/build/projects/pageRank/pageRank_xrt
+    echo "Executable for pagerank-pe1 already exists, skipping build"
 else
-    echo "Building pageRankv2_pe1_8"
-    cd "$ROOTDIR"/pageRankv2_pe1_8/src/host
+    echo "Building pagerank-pe1"
+    cd "$ROOTDIR"/pagerank-pe1/src/host
     mkdir -p build
     cd build
     cmake ..
@@ -27,8 +27,8 @@ enable_xilinx_2022.2
 
 
 
-# Run pageRankv2_pe1_8
-cd "$ROOTDIR"/pageRankv2_pe1_8
+# Run pagerank-pe1
+cd "$ROOTDIR"/pagerank-pe1
 for i in (seq 1 1)
     xbutil reset -d 01:00.1 --force
     bass "$ROOTDIR"/xbutil_power_logger.sh -i 0 -o power.txt -O output.txt -- ./src/host/build/projects/pageRank/pageRank_xrt ./xclbins/pageRankHS_hw.xclbin /tools/graphs/com-orkut.ungraph.txt
